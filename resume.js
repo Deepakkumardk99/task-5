8c28145538956da68af92b95892115d87c7fa722
Deepakkumardk99 <deepakkumarkingmaker@gmail.com>
2024-02-23 15:53:55 +0530

    resume

---
+++ resume.js
@@ -0,0 +1,48 @@
+let myResume = {
+  basics: {
+    name: "DEEPAK KUMAR K",
+    email: "deepakkumarkingmaker@gmail.com",
+    phone: 8248641225,
+    degree: "DME",
+    location: {
+      address: "42,POOLUVAPATTI,KAVINDAPATI",
+      postalCode: 638455,
+      city: "ERODE",
+      state: "Tamilnadu",
+      country: "India",
+    },
+    profiles: [
+      {
+        github: "https://github.com/Deepakkumardk99",
+      },
+    ],
+  },
+    
+  education: [
+    {
+      institution: "SAKTHI POLYTECHNIC COLLEGE",
+      department: "MECHANICAL ENGINEERING",
+      studyType: "fulltime",
+      "batch start year": 2017,
+      "batch end year": 2019,
+      overallPercentage:82 ,
+    },
+  ],
+  skills: [
+    {
+      name: "java script",
+      level: "beginer",
+    },
+  ],
+  languages: [
+    {
+      language: "Tamil,Enlish",
+    },
+  ],
+  interests: [
+    {
+      name: "traveling,gaming",
+    },
+  ],
+};
+console.log(myResume);
